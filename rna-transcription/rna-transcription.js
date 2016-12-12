3256f58f7a50834929140640ee1129db9d280e15
Nicholas J. Olson <nicholasjamesolson@gmail.com>
2016-12-11 22:12:38 -0600

    Transcribe DNA nucleotied Cytosine to RNA Guanine

---
+++ rna-transcription/rna-transcription.js
@@ -0,0 +1,19 @@
+class Transcriptor {
+  toRna(dnaStrand) {
+    let rnaStrand = ''
+    for (let i = 0; i < dnaStrand.length; i++) {
+      let nucleotide = dnaStrand.charAt(i)
+      rnaStrand += thing(nucleotide)
+    }
+    return rnaStrand
+  }
+}
+
+function thing(nucleotide) {
+  switch (nucleotide) {
+    case 'C':
+      return 'G'
+  }
+}
+
+export default Transcriptor;
